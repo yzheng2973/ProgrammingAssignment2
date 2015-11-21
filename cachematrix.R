85ac509dcbca130a3ae5f7f0953276f65f6c0a64
Yan Zheng <zhengyan531@hotmail.com>
2015-11-21 16:33:55 -0500

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix stores a list of functions. "Set" function stores the changes of the vector in the main function. m is set to inverse when the setmatrix function is called.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- NULL
+     set <- function(y){
+     	x <<- y
+     	m <<- NULL
+     }
+get <- function() x
+setmatrix <- function(solve) m <<- solve
+getmatrix <- function() m
+list(set = set, get = get, 
+     setmatrix = setmatrix, 
+     getmatrix = getmatrix)
+     
 }
 
 
-## Write a short comment describing this function
+## The input of cacheSlove is the object stored in the makeCacheMatrix function. It first check whether the inverse already calculated in the above function then it simply returns the value m; otherwise it will calculate the inverse for the new matrix.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
+	 m <- x$getmatrix()
+	 if(!is.null(m)){
+	 	 message("getting cache data")
+	 	 return(m)
+	 }
+	 matrix <- x$get()
+	 m<-solve(matrix, ...)
+	 x$setmatrix(m)
+	 m
         ## Return a matrix that is the inverse of 'x'
 }
+
+## Testing:
+a<- makeCacheMatrix()
+a$set(matrix(1:4,2,2))
+cacheSolve(a)
